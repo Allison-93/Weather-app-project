a220c2b1709a4ee70d0c593b08cc3b58d03f4012
Allison-93 <allisonmoopanar@gmail.com>
2024-09-28 16:15:37 +0200

    updated javascript functions

--- src/index.js
+++ src/index.js
@@ -2,8 +2,17 @@ function refreshWeather(response) {
   let temperatureElement = document.querySelector("#temperature");
   let temperature = response.data.temperature.current;
   let cityElement = document.querySelector("#current-city");
+  let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let windSpeedElement = document.querySelector("#wind-speed");
+  let timeElement = document.querySelector("#time");
+  let date = new Date(response.data.time * 1000);
   temperatureElement.innerHTML = Math.round(temperature);
   cityElement.innerHTML = response.data.city;
+  timeElement.innerHTML = `${date.getDay()},${date.getHours()}:${date.getMinutes()}`;
+  humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
+  windSpeedElement.innerHTML = `${response.data.wind.speed}km/h`;
+  descriptionElement.innerHTML = response.data.condition.description;
 }
 
 function searchCity(city) {
