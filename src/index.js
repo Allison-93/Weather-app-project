efcb73d1c05eec9fde722b162c6c083235a0d748
Allison-93 <allisonmoopanar@gmail.com>
2024-09-29 17:19:10 +0200

    injected weather data

--- src/index.js
+++ src/index.js
@@ -9,10 +9,29 @@ function refreshWeather(response) {
   let date = new Date(response.data.time * 1000);
   temperatureElement.innerHTML = Math.round(temperature);
   cityElement.innerHTML = response.data.city;
-  timeElement.innerHTML = `${date.getDay()},${date.getHours()}:${date.getMinutes()}`;
+  timeElement.innerHTML = formatDate(date);
   humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
   windSpeedElement.innerHTML = `${response.data.wind.speed}km/h`;
   descriptionElement.innerHTML = response.data.condition.description;
+}
+
+function formatDate(date) {
+  let minutes = date.getMinutes();
+  let hour = date.getHours();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  return `${day} ${hour}:${minutes}`;
 }
 
 function searchCity(city) {
